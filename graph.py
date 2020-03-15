cafd05b7549ae3c36dc8a6620fa349cb6cd09e9d
“Lindelanimcebo” <lindelanimcebo@gmail.com>
2020-03-15 14:15:46 +0200

    Graph working

--- graph.py
+++ graph.py
@@ -1,9 +1,26 @@
+import math
 
-for i in range(10):
-    for j in range(10):
-        if i==j:
-            print("*", end="")
-            
+f_x_str = input("Enter a function f(x):\n") #function as a string
+
+for y in range(10 , -11 , -1 ): #loop with y
+
+    for x in range(-10, 11):
+
+        f_x = eval(f_x_str)
+
+        if y == int(f_x):
+            print("o", end = '')
+
+        elif y == 0 and x == 0 :
+            print("+", end = '')
+
+        elif y == 0:
+            print("-", end = '')
+
+        elif x == 0:
+            print("|", end = '')
+
         else:
-            print(" ", end="")
+            print(" ", end = '')
+
     print()
